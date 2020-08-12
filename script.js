628a439b7683cc5b2aa73ef06770917d10c8063b
akash <aku.das02@gmail.com>
2020-08-12 07:33:22 +0600

    scramble list item

--- script.js
+++ script.js
@@ -14,12 +14,20 @@ createList();
 
 // Insert list items into DOM
 function createList() {
-    [...richestPeople].forEach((person, index) => {
-        const listItem = document.createElement('li');
+    [...richestPeople]
+    .map(a => ({
+            value: a,
+            sort: Math.random()
+        }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(a => a.value)
+        .forEach((person, index) => {
 
-        listItem.setAttribute('data-index', index);
+            const listItem = document.createElement('li');
 
-        listItem.innerHTML = `
+            listItem.setAttribute('data-index', index);
+
+            listItem.innerHTML = `
             <span class="number">${index + 1}</span>
             <div class="draggable" draggable="true">
                 <p class="person-name">${person}</p>
@@ -27,8 +35,8 @@ function createList() {
             </div>
         `;
 
-        listItems.push(listItem);
+            listItems.push(listItem);
 
-        draggable_list.appendChild(listItem);
-    });
+            draggable_list.appendChild(listItem);
+        });
 }
